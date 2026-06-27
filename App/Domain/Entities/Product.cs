b7956b3d84b9d81004990c4687b8d8fab9ca058a
Hovhannes Amirjanyan <hovhannes.amirjanyanit@gmail.com>
2026-06-27 17:19:00 +0400

    Price changed from decimal to int, code formating fixes

--- App/Domain/Entities/Product.cs
+++ App/Domain/Entities/Product.cs
@@ -5,7 +5,7 @@ public class ProductEntity
 {
     public int Id { get; set; }
     required public string Name { get; set; }
-    required public decimal Price { get; set; }
+    required public int Price { get; set; }
     public List<CartEntity> Carts { get; set; } = null!;
     public List<CheckoutEntity> Checkouts { get; set; } = null!;
     
